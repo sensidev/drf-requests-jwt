a1dfbe99684f90ca5490964056a9a22e21207c16
lucian <lucian.corduneanu@sensidev.com>
2019-05-28 13:14:22 +0300

    Add debug log on deserializer

--- drf_requests_jwt/deserializers.py
+++ drf_requests_jwt/deserializers.py
@@ -16,6 +16,7 @@ class ObjectListDeserializerMixin(object):
             raise Exception('`serializer_class` is required')
 
         for object_json in self.get_results_from_all_pages():
+            logger.debug("Serializing {}".format(object_json))
             serializer = self.serializer_class(data=object_json)
             if serializer.is_valid():
                 object_list.append(serializer.save())
